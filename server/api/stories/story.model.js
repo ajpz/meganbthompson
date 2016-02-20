208d140bd22e78a6ae614caec946f18f01d233ba
ajpz <perez.jonthan@gmail.com>
2016-02-20 11:41:57 -0500

    updated schema to allow for videoImg

--- server/api/stories/story.model.js
+++ server/api/stories/story.model.js
@@ -11,6 +11,7 @@ var storySchema = new mongoose.Schema({
   description: { type : String, required : true },
   type : { type : String, enum: ['documentary', 'journalism'], required: true },
   videoUri: { type: String, required : true, unique : true }, 
+  videoImg: { type: String },
   showName : { type : String, enum: ['newsHour', 'needToKnow', 'film', 'treasures','worldFocus'], required: true }
 }); 
 
