cbe6671df6bf30b939794b2fdc116f70c1b3832f
ajpz <perez.jonthan@gmail.com>
2016-02-20 11:44:34 -0500

    add new isolate scope variable ref to getVideoImg

--- browser/components/video-player/video-player.directive.js
+++ browser/components/video-player/video-player.directive.js
@@ -1,27 +1,24 @@
 app.directive('videoPlayer', function() {
   return {
     restrict: 'E', 
-    // templateUrl: '/components/video-player/video-player.html',
     template: '<div ng-include="templateHtml"></div>', 
     scope: {
-      // videoUrl: "@"
-      videoUrl: "&"
+      videoUrl: "&", 
+      videoImg: "&"
     }, 
     link: function(scope, element, attrs){
-      console.log('\n\nTEMPLATE CALLBACK\n'); 
-      console.log(scope.videoUrl(),'\n\n\n'); 
 
       scope.url = scope.videoUrl(); 
+      scope.img = scope.videoImg(); 
 
       var videoTemplate; 
 
       if(scope.url.indexOf('youtube') > -1) {
         // handle youtube
-        console.log('\n\nYOUTUBE IF\n\n\n')
         videoTemplate = '/components/video-player/video-player-youtube.html'; 
       } else if(scope.url.indexOf('thirteen') > -1) {
         //handle pbs
-        videoTemplate = '/components/video-player/video-player-pbs.html'; 
+        videoTemplate = '/components/video-player/video-player-thirteen.html'; 
       } else if(scope.url.indexOf('pbs') > -1) { 
         videoTemplate = '/components/video-player/video-player-pbs.html';
       }else if(scope.url.indexOf('vimeo') > -1) {
